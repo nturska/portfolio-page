0d606b465624fdf4436dd9be3ea20b42c3f21d45
Natalia Turska <natalia@MacBook-Pro-Natalia.local>
2026-09-23 15:49:12 +0200

    allow origin from cloudflare front

--- workout-api/main.py
+++ workout-api/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Workout Tracker API", version="1.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://nataliaturska.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
